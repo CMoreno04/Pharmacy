9c6282a0a0f9eaf5b913ec4d2a14bc509eddd765
Gean Hevia <gcaarlos30@gmail.com>
2023-09-12 21:25:20 -0400

    LoginPage component. Imports Loginform component, and makes use of Link component from react-router-dom for page routing/linking

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import LoginForm from "../../components/LoginForm/LoginForm";
+
+import "../../styles/flex-styles.css";
+import "./login-page-styles.css";
+
+const LoginPage = () => {
+  return (
+    <main className="main-container flex-container align-center justify-center">
+      <div className="login-form-container flex-container align-center flex-column">
+        <h1 className="login-h1">Sign in</h1>
+
+        <LoginForm />
+
+        <ul>
+          <li className="register-text">
+            Don't have an account yet?{" "}
+            <Link to="/register" className="link">
+              Sign up!
+            </Link>
+          </li>
+
+          <li className="access-help-text">
+            <Link to="/login" className="link">
+              Can't access your account?
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+};
+
+export default LoginPage;
