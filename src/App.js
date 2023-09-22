00dd245f56d3411db7e09f4a851664b3061942eb
Gean Hevia <gcaarlos30@gmail.com>
2023-09-21 20:45:17 -0400

    feature: Added LandingPage component to App.js routes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Routes } from "react-router-dom";
 
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import LandingPage from "./pages/LandingPage/LandingPage";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
     </Routes>
